ffa1afbc8f48b29406d7067197e65bdbfe372d08
Acasale <salestojanovic92@gmail.com>
2021-05-12 01:01:53 +0200

    uploadovan je domaci_12

---
+++ Nedelja_4/Sreda/12D_6126_Aleksandar_Stojanovic/domaci_12/zadatak_2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadatak 2</title>
+</head>
+<body>
+    <?php
+        $prva_boja = "Green";
+        $druga_boja = "Blue";
+
+        echo "<span style='color: green'>$prva_boja</span><br />";
+        echo "<span style='color: blue'>$druga_boja</span>";
+    ?>
+    <p>Za zadatak broj 3, kliknite na <a href="../zadatak_3/index.php" target=_blank>ovaj link</a></p>
+</body>
+</html>
